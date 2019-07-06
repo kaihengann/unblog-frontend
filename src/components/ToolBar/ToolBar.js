6c33053e4d8eb67b8683405a5761b09ade83a426
kaihengann <annkaiheng@gmail.com>
2019-07-06 21:52:01 +0800

    Add ToolBar

---
+++ src/components/ToolBar/ToolBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import StyleButton from "../StyleButton/StyleButton";
+
+const BLOCK_TYPES = [
+  { label: "H1", style: "header-one" },
+  { label: "H2", style: "header-two" },
+  { label: "H3", style: "header-three" },
+  { label: "Blockquote", style: "blockquote" },
+  { label: "UL", style: "unordered-list-item" },
+  { label: "OL", style: "ordered-list-item" },
+  { label: "Code Block", style: "code-block" }
+];
+
+const ToolBar = ({ editorState, onToggle }) => {
+  const selection = editorState.getSelection();
+  const blockType = editorState
+    .getCurrentContent()
+    .getBlockForKey(selection.getStartKey())
+    .getType();
+
+  return (
+    <div className="toolBar">
+      {BLOCK_TYPES.map(type => (
+        <StyleButton
+          key={type.label}
+          active={type.style === blockType}
+          label={type.label}
+          onToggle={onToggle}
+          style={type.style}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ToolBar;
