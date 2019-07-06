01ccfb5be5b4cb760cf4a48b38db6b185f8f7518
kaihengann <annkaiheng@gmail.com>
2019-07-06 10:01:32 +0800

    Remove unused prop

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NavBar = ({ onClick, onLogout }) => {
+const NavBar = ({ onLogout }) => {
   return (
     <nav>
       <button>
