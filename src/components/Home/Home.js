457b0e9cbe368ed717a77d4f4e014921549d4a4c
kaihengann <annkaiheng@gmail.com>
2019-07-07 15:08:09 +0800

    Add blogpost cards and routing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import moment from "moment";
+import { withRouter } from "react-router-dom";
+import "./Home.css";
 
-const Home = () => {
-  return (
-    <p>This is Home</p>
-  );
-};
+const Home = withRouter(({ posts, history, onClick }) => {
+  if (posts) {
+    const blogPosts = posts.map(
+      ({ postId, postTitle, createdOn, updatedOn }) => {
+        const dateCreated = moment(createdOn).format("MMMM D YYYY, h:mm a");
+        return (
+          <div
+            className="blogPost"
+            id={postId}
+            key={postId}
+            onClick={() => {
+              history.push(`/posts/${postId}`);
+            }}
+          >
+            <div>
+              <h4>{dateCreated}</h4>
+              <h1>{postTitle}</h1>
+            </div>
+          </div>
+        );
+      }
+    );
+
+    return <div>{blogPosts}</div>;
+  }
+  return null;
+});
 
 export default Home;
